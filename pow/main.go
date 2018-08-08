63c6ef1f793dac93700d85af6c445aef135c5d21
OhBonsai <letbonsaibe@gmail.com>
2018-08-08 23:24:18 +0800

    update main

--- pow/main.go
+++ pow/main.go
@@ -14,7 +14,7 @@ func main(){
 	for _, b := range bc.blocks {
 		fmt.Printf("Prev.hash is %x \n", b.PrevBlockHash)
 		fmt.Printf("Data is %s \n", b.Data)
-		fmt.Printf("Hash is %x \n", b.PrevBlockHash)
+		fmt.Printf("Hash is %x \n", b.Hash)
 		pow := NewProofOfWork(b)
 		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
 
--- prototype/main.go
+++ prototype/main.go
@@ -11,7 +11,7 @@ func main(){
 	for _, b := range bc.blocks {
 		fmt.Printf("Prev.hash is %x \n", b.PrevBlockHash)
 		fmt.Printf("Data is %s \n", b.Data)
-		fmt.Printf("Hash is %x \n", b.PrevBlockHash)
+		fmt.Printf("Hash is %x \n", b.Hash)
 
 	}
 }
